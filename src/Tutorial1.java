c20a01abb49717e762891bb51eec0a67736f75ad
shaahid fawzi <shaahidbasic101@gmail.com>
2020-09-05 10:05:41 +0530

    mades coupls changes

--- src/Tutorial1.java
+++ src/Tutorial1.java
@@ -4,106 +4,108 @@ import java.util.Scanner;
 public class Tutorial1 {
 
     public static void main(String[] args) {
-        System.out.println("Question 1");//question 1
+//              System.out.println("Question 1");//question 1
+//
+//
+//        System.out.print("Chinthana Changed this line");
+//        System.out.print("added a new line");
+//
+//        System.out.println("Hello World\n");
+//
+//        System.out.println("Question 2");//question 2
+//
+//        System.out.println("Shaahid\n");
+//
+//        System.out.println("Question 3");//question 3
+//
+//        Scanner height = new Scanner(System.in);
+//        System.out.println("Enter your height");
+//        int height_input = height.nextInt();
+//        System.out.println("height: "+height_input+"\n");
+//
+//        System.out.println("Question 4");//question 4
+//
+//        Scanner color = new Scanner(System.in);
+//        System.out.println("Enter your favorite colour");
+//        String color_input = color.nextLine();
+//        System.out.println("height: "+color_input+"\n");
+//
+//        System.out.println("Question 5");//question 5
+//
+//        System.out.println("Var2=='Hi' is incorrect\n");
+//
+//        System.out.println("Question 6");//question 6
+//
+//        int w = 5;
+//        int x = 4;
+//        int y = 8;
+//        int z = 2;
+//
+//        System.out.println("a. "+x+y);
+//        System.out.println("b. "+z*2);
+//        System.out.println("c. "+y/x);
+//        System.out.println("d. "+(y-z));
+//        System.out.println("e. "+w%z+"\n");
+//
+//        System.out.println("Question 7");//question 7
+//
+//        int total = 10 + 14;
+//        System.out.println(total+"\n");
+//
+//        System.out.println("Question 8");//question 8
+//
+//        int down_payment = 5;
+//        int due = down_payment-total;
+//        System.out.println("The due payment is "+due+"\n");
+//
+//        System.out.println("Question 9");//question 9
+//
+//        Scanner subtotal_input = new Scanner(System.in);
+//        System.out.println("Enter the subtotal ");
+//        int subtotal = subtotal_input.nextInt();
+//        double totals = subtotal* 0.15;
+//
+//        System.out.println("The total is " + totals+"\n");
+//
+//        System.out.println("Question 10");//question 10
+//
+//        int a = 5;
+//        int b = 2;
+//        int c = 3;
+//
+//        int result = a+(b*c);
+//        System.out.println(result+"\n");
+//
+//        System.out.println("Question 11");//question 11
+//
+//        int a1=20;
+//        int b1=40;
+//        int c1=30;
+//
+//        //(x,y,z) = (a1,b1,c1);
+//        //System.out.println(x,y,z);
+//        System.out.println("check this in the code\n");
+//
+//
+//        System.out.println("Question 12");//question 12
+//
+//        double sales = 50.68547;
+//        DecimalFormat decimal_formatter = new DecimalFormat("##.000");
+//        System.out.println(decimal_formatter.format(sales));
+//
+//        System.out.println("Question 13");//question 13
+//
+//        double number = 1234567.456;
 
+        //post and pre increment
+        int x = 5;
+        int y = 5;
 
-        System.out.print("Chinthana Changed this line");
-        System.out.print("added a new line");
+        System.out.println(x++);//prints then adds 1
+        System.out.println(x);
 
-        System.out.println("Hello World\n");
-
-        System.out.println("Question 2");//question 2
-
-        System.out.println("Shaahid\n");
-
-        System.out.println("Question 3");//question 3
-
-        Scanner height = new Scanner(System.in);
-        System.out.println("Enter your height");
-        int height_input = height.nextInt();
-        System.out.println("height: "+height_input+"\n");
-
-        System.out.println("Question 4");//question 4
-
-        Scanner color = new Scanner(System.in);
-        System.out.println("Enter your favorite colour");
-        String color_input = color.nextLine();
-        System.out.println("height: "+color_input+"\n");
-
-        System.out.println("Question 5");//question 5
-
-        System.out.println("Var2=='Hi' is incorrect\n");
-
-        System.out.println("Question 6");//question 6
-
-        int w = 5;
-        int x = 4;
-        int y = 8;
-        int z = 2;
-
-        System.out.println("a. "+x+y);
-        System.out.println("b. "+z*2);
-        System.out.println("c. "+y/x);
-        System.out.println("d. "+(y-z));
-        System.out.println("e. "+w%z+"\n");
-
-        System.out.println("Question 7");//question 7
-
-        int total = 10 + 14;
-        System.out.println(total+"\n");
-
-        System.out.println("Question 8");//question 8
-
-        int down_payment = 5;
-        int due = down_payment-total;
-        System.out.println("The due payment is "+due+"\n");
-
-        System.out.println("Question 9");//question 9
-
-        Scanner subtotal_input = new Scanner(System.in);
-        System.out.println("Enter the subtotal ");
-        int subtotal = subtotal_input.nextInt();
-        double totals = subtotal* 0.15;
-
-        System.out.println("The total is " + totals+"\n");
-
-        System.out.println("Question 10");//question 10
-
-        int a = 5;
-        int b = 2;
-        int c = 3;
-
-        int result = a+(b*c);
-        System.out.println(result+"\n");
-
-        System.out.println("Question 11");//question 11
-
-        int a1=20;
-        int b1=40;
-        int c1=30;
-
-        //(x,y,z) = (a1,b1,c1);
-        //System.out.println(x,y,z);
-        System.out.println("check this in the code\n");
-
-
-        System.out.println("Question 12");//question 12
-
-        double sales = 50.68547;
-        DecimalFormat format = new DecimalFormat("##.00");
-        System.out.println(Math.round(Float.parseFloat(format.format(sales))));
-
-        System.out.println("Question 13");//question 13
-
-        double number = 1234567.456;
-
-
-
-
-
-
-
-
+        System.out.println(++y);//before executing this line add 1 to y
+        System.out.println(y);
 
 
     }
